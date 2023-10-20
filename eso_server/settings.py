b04a8a99c2b8195e6f9c943aeaeb71eedeae96bf
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-10-19 21:30:11 -0400

    Adding postgres db to the project.

--- eso_server/settings.py
+++ eso_server/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'eso_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eso_db_dev',
+        'USER': 'esodev',
+        'PASSWORD': 'esodevpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
